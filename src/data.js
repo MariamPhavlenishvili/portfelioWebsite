0567ce8f650478a79c63f9c5a3bd1b77b9635c69
MariamPhavlenishvili <mariam.pavlenishvili.1@btu.edu.ge>
2022-06-02 19:29:16 +0400

    Deploy

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "Remote Jobs App",
+    subtitle: "Laravel Project",
+    description: "Users can upload remote jobs or find one",
+    image:
+      "https://media.wired.com/photos/621575377d11d746344b5a72/master/pass/Business_Person%20Working%20on%20Laptop_1302475706.jpg",
+    link: "https://reactboothttps://github.com/MariamPhavlenishvili/remote_Jobs_Portal/tree/main/jobsPortalcamp.com",
+  },
+  {
+    title: "TODO List App",
+    subtitle: "Laravel Project",
+    description: "App for users to create or update their todo lists",
+    image: "http://www.workflowmax.com/hubfs/6-things-to-do-list.png",
+    link: "https://github.com/MariamPhavlenishvili/todoList-rest-api",
+  },
+  {
+    title: "Charts App",
+    subtitle: "React Project",
+    description: "Creating charts in react",
+    image:
+      "https://i.pinimg.com/originals/e1/c6/72/e1c672de0954214b11319bc31b9fa4c0.png",
+    link: "https://github.com/MariamPhavlenishvili/React-Tutorial-Charts",
+  },
+  {
+    title: "Example react router",
+    subtitle: "React Project",
+    description: "Test project to see how react router version 6 works",
+    image:
+      "https://www.kindpng.com/picc/m/296-2968253_1460px-react-logo-react-native-logo-png-transparent.png",
+    link: "https://github.com/MariamPhavlenishvili/ReactRouterV6",
+  },
+];
+
+export const skills = [
+  "PHP / Laravel",
+  "Python / Numpy / Pandas",
+  "Javascript / React",
+  "HTML/CSS",
+  "GIT",
+  "Amazon Web Services",
+];
